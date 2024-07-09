596195e313c6f54444164ec2883800f08c573787
LRCode20 <86530453+LRCode20@users.noreply.github.com>
2024-07-09 10:04:02 -0600

    se mejoro la validacion al password con 6 caracteres minimo

--- js/main.js
+++ js/main.js
@@ -8,14 +8,15 @@
   const passwordHtml = document.getElementById('password');
 
   loginBtn.addEventListener('click', (evento) => {
-    console.log(usuarioHtml.value)
-    if (usuarioHtml.value != "" && emailHtml.value != "" && passwordHtml.value != "") {
-      alert('Bienvenido  ' + usuarioHtml.value + ', gracias por acceder al sistema')
+    console.log(usuarioHtml.value);
+    if (usuarioHtml.value !== "" && emailHtml.value !== "" && passwordHtml.value !== "") {
+      if (passwordHtml.value.length >= 6) {
+        alert('Bienvenido ' + usuarioHtml.value + ', gracias por acceder al sistema');
+      } else {
+        alert('La contraseña debe tener al menos 6 caracteres');
+      }
     } else {
-      alert(' Es requerido ingresar usuario, email y contraseña')
+      alert('Es requerido ingresar usuario, email y contraseña');
     }
-
-  })
-
+  });
 })();
-
